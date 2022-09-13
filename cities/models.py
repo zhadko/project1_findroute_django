3ca76e5ae5013525538204b0c3d0c9bf8f7e282e
corrrvo <nazar.yurie@gmail.com>
2022-09-13 19:35:39 +0300

    added model 'City'

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+        ordering = ['name']
