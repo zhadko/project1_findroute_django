f28707ac1d8558ff7138001462154cc61932cb0f
corrrvo <nazar.yurie@gmail.com>
2022-10-28 20:49:54 +0300

    Add to function to Procfile

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
